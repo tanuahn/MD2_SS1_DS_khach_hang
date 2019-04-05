63a99c9e3f183bdde2a58e9045838e4631a6f21f
tanuahn <thieulatuananh92@gmail.com>
2019-04-05 10:45:06 +0700

    DSKH

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+    th, td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+</style>
+<body>
+<table border="0">
+    <caption><h1>Danh sách khách hàng</h1></caption>
+    <tr>
+        <th>STT</th>
+        <th>Tên</th>
+        <th>Ngày sinh</th>
+        <th>Địa chỉ</th>
+        <th>Ảnh</th>
+    </tr>
+</table>
+</body>
+</html>
+<?php
+$customerlist = array(
+    "1" => array("ten" => "Mai Văn Hoàn",
+        "ngaysinh" => "1983-08-20",
+        "diachi" => "Hà Nội",
+        "anh" => "1.jpeg"),
+    "2" => array("ten" => "Nguyễn Văn Nam",
+        "ngaysinh" => "1983-08-20",
+        "diachi" => "Bắc Giang",
+        "anh" => "2.jpeg"),
+    "3" => array("ten" => "Nguyễn Thái Hòa",
+        "ngaysinh" => "1983-08-21",
+        "diachi" => "Nam Định",
+        "anh" => "3.jpeg"),
+    "4" => array("ten" => "Trần Đăng Khoa",
+        "ngaysinh" => "1983-08-22",
+        "diachi" => "Hà Tây",
+        "anh" => "4.jpeg"),
+    "5" => array("ten" => "Nguyễn Đình Thi",
+        "ngaysinh" => "1983-08-17",
+        "diachi" => "Hà Nội",
+        "anh" => "5.jpeg")
+);
+
+foreach($customerlist as $key => $values){
+    echo "<tr>";
+    echo "<td>".$key."</td>";
+    echo "<td>".$values['ten']."</td>";
+    echo "<td>".$values['ngaysinh']."</td>";
+    echo "<td>".$values['diachi']."</td>";
+    echo "<td><image src ='".$values['anh']."' width = '60px' height ='60px'/></td>";
+    echo "</tr>";
+}
+?>
